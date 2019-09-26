8e72a768a76b3ea0237ac2895fec4bbf421217d7
phuocphn <phuocphn@gmail.com>
2019-09-26 17:03:27 +0900

    fix correct shape in conv2d func

--- models.py
+++ models.py
@@ -2,19 +2,18 @@ import tensorflow as tf
 import numpy as np
 
 # Create some wrappers for simplicity
-def conv2d(name, input, strides=[2, 2], filter_size=[3, 3], num_filters=32):
+def conv2d(variable_namescope, input, strides=[2, 2], filter_size=[3, 3], num_filters=32):
     # Conv2D wrapper, with bias and relu activation
-    with tf.variable_scope(name):
-        W = tf.get_variable("W", shape=[filter_size[0], filter_size[1], int(input.shape()[3]), num_filters],
+    with tf.variable_scope(variable_namescope):
+        W = tf.get_variable("W", shape=[filter_size[0], filter_size[1], int(input.get_shape()[3]), num_filters],
                             dtype=tf.float32,
                             # https://medium.com/@prateekvishnu/xavier-and-he-normal-he-et-al-initialization-8e3d7a087528
                             initializer=tf.contrib.layers.variance_scaling_initializer(factor=1.0, mode='FAN_IN',
                                                                                        uniform=True, seed=None,
                                                                                        dtype=tf.float32))
-        b = tf.get_variable("b", shape=[1, 1, 1, 32], dtype=tf.float32,
+        b = tf.get_variable("b", shape=[1, 1, 1, num_filters], dtype=tf.float32,
                             initializer=tf.constant_initializer(0.0))
-        return tf.nn.elu(
-            tf.nn.bias_add(tf.nn.conv2d(input, filter=W, strides=[1, strides[0], strides[1], 1], padding="SAME"), b))
+        return tf.nn.elu(tf.nn.conv2d(input, filter=W, strides=[1, strides[0], strides[1], 1], padding="SAME") + b)
 
 
 def normalized_columns_initializer(std=1.0):
@@ -40,10 +39,45 @@ class CNNLSTMPolicy(object):
         # 4 convolution layer stack together.
         __input = self.input
         for idx in range(4):
-            __input = conv2d(name=f"layer_{idx}", input=__input, strides=[2,2], filter_size=[3,3], num_filters=32)
+            __input = conv2d(variable_namescope="layer_%s" % idx, input=__input, strides=[2,2], filter_size=[3,3], num_filters=32)
         self.output = tf.reshape(__input, [-1, np.prod(__input.get_shape().as_list()[1:])])
 
         # Last fully connected layer (value function).
-        w = tf.get_variable("value_function/w", [__input.get_shape()[1], num_action], initializer=normalized_columns_initializer(1.0))
+        w = tf.get_variable("value_function/w", [self.output.get_shape()[1], num_action], initializer=normalized_columns_initializer(1.0))
         b = tf.get_variable("value_function/b", [num_action], initializer=tf.constant_initializer(0.0))
-        self.logits = tf.matmul(__input, w) + b
+        self.logits = tf.matmul(self.output, w) + b
+
+
+
+"""
+def create_network(session, available_actions_count):
+    # Create the input variables
+    s1_ = tf.placeholder(tf.float32, [None] + list(resolution) + [1], name="State")
+    a_ = tf.placeholder(tf.int32, [None], name="Action")
+    target_q_ = tf.placeholder(tf.float32, [None, available_actions_count], name="TargetQ")
+
+    # Add 2 convolutional layers with ReLu activation
+    conv1 = tf.contrib.layers.convolution2d(s1_, num_outputs=8, kernel_size=[6, 6], stride=[3, 3],
+                                            activation_fn=tf.nn.relu,
+                                            weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                            biases_initializer=tf.constant_initializer(0.1))
+    conv2 = tf.contrib.layers.convolution2d(conv1, num_outputs=8, kernel_size=[3, 3], stride=[2, 2],
+                                            activation_fn=tf.nn.relu,
+                                            weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                            biases_initializer=tf.constant_initializer(0.1))
+    conv2_flat = tf.contrib.layers.flatten(conv2)
+    fc1 = tf.contrib.layers.fully_connected(conv2_flat, num_outputs=128, activation_fn=tf.nn.relu,
+                                            weights_initializer=tf.contrib.layers.xavier_initializer(),
+                                            biases_initializer=tf.constant_initializer(0.1))
+
+    q = tf.contrib.layers.fully_connected(fc1, num_outputs=available_actions_count, activation_fn=None,
+                                          weights_initializer=tf.contrib.layers.xavier_initializer(),
+                                          biases_initializer=tf.constant_initializer(0.1))
+    best_a = tf.argmax(q, 1)
+
+    loss = tf.losses.mean_squared_error(q, target_q_)
+
+    optimizer = tf.train.RMSPropOptimizer(learning_rate)
+    # Update the parameters according to the computed gradient using RMSProp.
+    train_step = optimizer.minimize(loss)
+"""
---
+++ tests/env.py
@@ -0,0 +1,86 @@
+import itertools as it
+from random import choice
+from time import sleep
+import vizdoom as vzd
+import skimage
+import numpy as np
+from models import CNNLSTMPolicy
+import tensorflow as tf
+
+def preprocess(img, resolution=(84, 84)):
+    return np.asarray(skimage.transform.resize(img, resolution).astype(np.float32))
+
+if __name__ == "__main__":
+
+    game = vzd.DoomGame()
+    # Choose scenario config file you wish to watch.
+    # Don't load two configs cause the second will overwrite the first one.
+    # Multiple config files are ok but combining these ones doesn't make much sense.
+    game.load_config("scenarios/my_way_home.cfg")
+
+    # Makes the screen bigger to see more details.
+    game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
+    game.set_window_visible(True)
+    game.add_game_args('+"bind w +forward" +"bind s +back" +"bind a +left" +"bind d +right"')
+
+    # game.set_mode(vzd.Mode.SPECTATOR)
+    game.init()
+
+    # Creates all possible actions depending on how many buttons there are.
+    actions_num = game.get_available_buttons_size()
+    print ("Number of actions: ", actions_num)
+    print ("""
+    available_buttons = 
+        { 
+            TURN_LEFT
+            TURN_RIGHT
+            MOVE_FORWARD 
+            MOVE_LEFT
+            MOVE_RIGHT
+        }
+    """)
+
+    actions = [list(a) for a in it.product([0, 1], repeat=actions_num)]
+
+    print ("****" * 10)
+    print ("Length combination of actions: ", len(actions))
+    assert 2**actions_num == len(actions)
+    print ("****" * 10)
+
+    cnnlstm_model = CNNLSTMPolicy(state_shape=[84, 84, 3], num_action=actions_num)
+
+    episodes = 10
+    sleep_time = 0.028
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for i in range(episodes):
+            print("Episode #" + str(i + 1))
+
+            # Not needed for the first episode but the loop is nicer.
+            game.new_episode()
+            while not game.is_episode_finished():
+
+                # Gets the state and possibly to something with it
+                state = game.get_state()
+                __state = preprocess(state.screen_buffer) #numpy array with shape (84,84,3)
+
+                logits = sess.run([cnnlstm_model.logits], feed_dict={cnnlstm_model.input: [__state] })
+                print ("Logits: ", logits)
+
+                # Makes a random action and save the reward.
+                reward = game.make_action(choice(actions))
+
+                print("Game Variables:", state.game_variables)
+                print("Performed action:", game.get_last_action())
+                print("Last Reward:", reward)
+                print("=====================")
+
+                # Sleep some time because processing is too fast to watch.
+                if sleep_time > 0:
+                    sleep(sleep_time)
+
+            print("Episode finished!")
+            print("total reward:", game.get_total_reward())
+            print("************************")
