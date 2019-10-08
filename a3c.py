fix collecting gradients errors

a3c.py
@@ -3,7 +3,6 @@ from policys import CNNLSTMPolicy, StateActionPredictor
 import scipy.signal
 import numpy as np
 import skimage
-from tqdm import tqdm
 
 class A3C(object):
     def __init__(self, env, worker_task_index, sess=None):
@@ -58,11 +57,14 @@ class A3C(object):
 
             # ICM
             self.predict_loss = 10.0 * (self.local_state_action_predictor.invese_loss * (1-0.2) + self.local_state_action_predictor.forward_loss * 0.2)
-            predict_gradients = tf.gradients(self.predict_loss * 20.0, self.local_state_action_predictor.var_list)
+            #self.predict_loss = 10.0 * (self.local_state_action_predictor.invese_loss )
 
+            predict_gradients = tf.gradients(self.predict_loss * 20.0, self.local_state_action_predictor.var_list)
+            print ("$"*100)
+            print (self.local_state_action_predictor.var_list)
             print ("*"*100)
             print (predict_gradients)
-
+            #exit()
             tf.summary.scalar("model/policy_loss", policy_loss )
             tf.summary.scalar("model/value_loss", value_function_loss )
             tf.summary.scalar("model/entropy", entropy_loss)
@@ -77,17 +79,35 @@ class A3C(object):
 
             self.summary_op = tf.summary.merge_all()
 
-            gradients, gradient_norms = tf.clip_by_global_norm(t_list=gradients,clip_norm=40.0)
+            gradients, gradient_norms = tf.clip_by_global_norm(gradients,clip_norm=40.0)
             grads_and_vars = list(zip(gradients, self.global_network.var_list))
             if True: # use ICM
-                predict_gradients = tf.clip_by_global_norm(t_list=predict_gradients, clip_norm=40.0)
+                print ("before   ---- predict_gradients: " )
+                print ("*"*100)
+                print (predict_gradients)
+                print ("*"*100)
+
+
+                predict_gradients, _ = tf.clip_by_global_norm(predict_gradients, clip_norm=40.0)
                 predict_gradients_and_vars = list(zip(predict_gradients, self.global_state_action_predictor.var_list))
+                print ("predict_gradients: " )
+                print ("*"*100)
+                print (predict_gradients)
+                print ("*"*100)
+                print ("predict_gradients_and_vars: " )
+                print ("*"*100)
+                print (predict_gradients_and_vars)
+                print ("*"*100)
+
                 print (predict_gradients_and_vars)
                 print ("#"*1000)
                 print (grads_and_vars)
                 print ("#"*1000)
-
-                #grads_and_vars = grads_and_vars + predict_gradients_and_vars
+                #ex_delta_t = tf.reduce_mean(tf.concat([tf.reshape(g, [-1]) for g in tf.gradients(t
+                grads_and_vars = grads_and_vars + predict_gradients_and_vars
+                print ("*"*100)
+                print (grads_and_vars)
+                #exit()
 
             optimizer = tf.train.AdamOptimizer(learning_rate=float(1e-4))
             self.train_op = tf.group(optimizer.apply_gradients(grads_and_vars=grads_and_vars,

policys.py
@@ -27,8 +27,8 @@ def FeatureExtractor(inputs):
                                              weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                                              biases_initializer=tf.constant_initializer(0.1))
 
-    with tf.variable_scope("output_flatten"):
-        output = tf.reshape(conv_4, [-1, np.prod(conv_4.get_shape().as_list()[1:])])
+    with tf.variable_scope("flatten_output"):
+        output = tf.reshape(conv_4, [-1, np.prod(conv_4.get_shape().as_list()[1:])], name="reshaped_flatten_output")
 
     return output
 
@@ -135,10 +135,10 @@ class StateActionPredictor(object):
         g = tf.concat([phi1, phi2], 1 )
         g = tf.contrib.layers.fully_connected(g, num_outputs=256, activation_fn=tf.nn.relu,
                                                     weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                                    biases_initializer=tf.constant_initializer(0.1))
+                                                    biases_initializer=tf.constant_initializer(0.1), scope="inverse_fully_connected_layer_1")
         logits = tf.contrib.layers.fully_connected(g, num_outputs=ac_space, activation_fn=None,
                                                     weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                                    biases_initializer=tf.constant_initializer(0.1))
+                                                    biases_initializer=tf.constant_initializer(0.1), scope="inverse_fully_connected_layer_2")
         action_indexes = tf.argmax(action_sample, axis=1)
         self.invese_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=action_indexes), name="inverse_loss")
         self.action_inverse_probs = tf.nn.softmax(logits, dim=-1)
@@ -148,11 +148,11 @@ class StateActionPredictor(object):
         f = tf.concat([phi1, action_sample], 1)
         f = tf.contrib.layers.fully_connected(f, num_outputs=256, activation_fn=tf.nn.relu,
                                                     weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                                  biases_initializer=tf.constant_initializer(0.1))
+                                                  biases_initializer=tf.constant_initializer(0.1), scope="forward_fully_connected_layer_1")
 
         f = tf.contrib.layers.fully_connected(f, num_outputs=phi1.get_shape()[1].value, activation_fn=None,
                                                     weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                                    biases_initializer=tf.constant_initializer(0.1))
+                                                    biases_initializer=tf.constant_initializer(0.1), scope="forward_fully_connected_layer_2")
         self.forward_loss = 0.5 * tf.reduce_mean(tf.square(tf.subtract(f, phi2)), name="forward_loss")
         self.forward_loss *= 288.0
 

train.py
@@ -30,7 +30,7 @@ print ("env.spec.timestep_limit: ", env.spec.tags.get('wrapper_config.TimeLimit.
 print ("*" * 50)
 time.sleep(5)
 
-cluster = tf.train.ClusterSpec({"ps": ["localhost:12200"], "worker": ["localhost:12300", "localhost:12301", "localhost:12302", "localhost:12303"]})
+cluster = tf.train.ClusterSpec({"ps": ["localhost:12200"], "worker": ["localhost:{}".format(port) for port in range(12300, 12500, 1)]})
 if JOB_NAME == 'ps':
     #os.system("kill -9 $( lsof -i:12200 -t ) > /dev/null 2>&1")
     server = tf.train.Server(server_or_cluster_def=cluster, job_name=JOB_NAME, task_index=0,